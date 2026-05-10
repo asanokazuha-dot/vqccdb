1f1ec3976acf3c941e3d8028036e664c30bb379a
onli00312-byte <onli00312@gmail.com>
2026-05-10 12:25:35 +0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@ const RESULT_KEY = 'vxcc-wheel-result'
 
 const segments = [
   { label: 'Voucher 5.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fce4ec', textColor: '#c9184a' },
-  { label: 'Voucher 15.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fff0f3', textColor: '#e91e63' },
+  { label: 'Voucher 20.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fff0f3', textColor: '#e91e63' },
   { label: 'Voucher 15.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fce4ec', textColor: '#c9184a' },
   { label: 'Voucher 10.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fff0f3', textColor: '#e91e63' },
   { label: 'Voucher 35.000.000 Vnd', img: '/img/vxcc/voucher.png', color: '#fce4ec', textColor: '#c9184a' },
